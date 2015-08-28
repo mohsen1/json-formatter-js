ab335da5f593f4dc8b067a475b064a0cc05a89e9
Mohsen Azimi <me@azimi.me>
2015-08-27 18:29:22 -0700

    Add some of the test

--- test/spec.js
+++ test/spec.js
@@ -7,70 +7,67 @@
 describe('rendering', ()=> {
   describe('null', ()=> {
     it('should render "null"', function () {
-      const element = new JSONFormatter(null);
+      const formatter = new JSONFormatter(null);
 
-      expect(element.render().innerText).to.contain('null');
+      expect(formatter.render().innerText).to.contain('null');
     });
   });
+
+  describe('undefined', ()=> {
+    it('should render "undefined"', ()=> {
+      const formatter = new JSONFormatter(undefined);
+
+      expect(formatter.render().innerText).to.contain('undefined');
+    });
+  });
+
+  describe('function', ()=> {
+    it('should render the function', ()=> {
+      const formatter = new JSONFormatter(function add(a, b) { return a + b; });
+      const elementText = formatter.render().innerText;
+
+      expect(elementText).to.contain('function');
+      expect(elementText).to.contain('add');
+      expect(elementText).to.contain('(a, b)');
+      expect(elementText.trim().match(/function\s[^\(]*\([^\)]*\)\s*(.*)/)[1]).to.equal('{…}');
+    });
+  });
+
+  describe('string', ()=> {
+    it('should render "Hello World!"', ()=> {
+      const formatter = new JSONFormatter('Hello World!');
+
+      expect(formatter.render().innerText).to.contain('Hello World');
+    });
+  });
+
+  describe('date string', ()=> {
+    const formatter = new JSONFormatter(new Date(0).toString());
+
+    it('should render "' + (new Date(0)).toString() + '"', ()=> {
+      expect(formatter.render().innerText).to.contain('"' + (new Date(0)).toString() + '"');
+    });
+
+    it('add the "date" class', ()=> {
+
+      expect(formatter.render().querySelector('.date')).not.to.equal(null);
+    });
+  });
+
+  describe('url string', function(){
+    const formatter = new JSONFormatter('https://example.com');
+
+    it('should render "https://example.com"', function () {
+      expect(formatter.render().innerText).to.contain('"https://example.com"');
+    });
+
+    it('should make a link and add class "url"', function() {
+      expect(formatter.render().querySelector('a.url')).not.to.equal(null);
+    });
+  });
+
 });
 
-  // describe('undefined', function(){
-  //   it('should render "undefined"', function () {
-  //     element = createDirective('_undefined');
-  //     expect(element.text()).toContain('undefined');
-  //   });
-  // });
-
-  // describe('function', function(){
-  //   it('should render the function', function () {
-  //     element = createDirective('_function');
-  //     expect(element.text()).toContain('function');
-  //     expect(element.text()).toContain('add');
-  //     expect(element.text()).toContain('(a, b)');
-  //     expect(element.text().trim().match(/function\s[^\(]*\([^\)]*\)\s*(.*)/)[1]).toBe('{…}');
-  //   });
-  // });
-
-  // describe('promiseFunction', function(){
-  //   it('should render the function', function () {
-  //     element = createDirective('promiseFunction');
-  //     expect(element.text()).toContain('function');
-  //     expect(element.text()).toContain('getAdd');
-  //     expect(element.text()).toContain('(service, a)');
-  //     expect(element.text().trim().match(/function\s[^\(]*\([^\)]*\)\s*(.*)/)[1]).toBe('{…}');
-  //   });
-  // });
-
-  // describe('string', function(){
-  //   it('should render "Hello world!"', function () {
-  //     element = createDirective('string');
-  //     expect(element.text()).toContain('"Hello world!"');
-  //   });
-  // });
-
-  // describe('date string', function(){
-  //   beforeEach(function(){
-  //     element = createDirective('date');
-  //   });
-  //   it('should render "' + (new Date(0)).toString() + '"', function () {
-  //     expect(element.text()).toContain('"' + (new Date(0)).toString() + '"');
-  //   });
-  //   it('should add "date" class to string', function() {
-  //     expect(element.find('span.date').length).toBe(1);
-  //   });
-  // });
-
-  // describe('url string', function(){
-  //   beforeEach(function(){
-  //     element = createDirective('url');
-  //   });
-  //   it('should render "https://example.com"', function () {
-  //     expect(element.text()).toContain('"https://example.com"');
-  //   });
-  //   it('should add "url" class to string', function() {
-  //     expect(element.find('span.url').length).toBe(1);
-  //   });
-  // });
 
   // describe('empty object', function(){
   //   testEmptyObject('emptyObject');
@@ -87,7 +84,7 @@ describe('rendering', ()=> {
   //       element = createDirective(key);
   //     });
   //     it('should render "Object"', function() {
-  //       expect(element.text()).toContain('Object');
+  //       expect(formatter.render().innerText).to.contain('Object');
   //     });
   //   });
   //   describe('with open="1"', function() {
@@ -95,10 +92,10 @@ describe('rendering', ()=> {
   //       element = createDirective(key, 1);
   //     });
   //     it('should render "Object"', function() {
-  //       expect(element.text()).toContain('Object');
+  //       expect(formatter.render().innerText).to.contain('Object');
   //     });
   //     it('should render have toggler opened', function() {
-  //       expect(element.find('.toggler').hasClass('open')).toBe(true);
+  //       expect(element.find('.toggler').hasClass('open')).to.equal(true);
   //     });
   //   });
   // }
@@ -110,7 +107,7 @@ describe('rendering', ()=> {
 
   //   it('should render "" for key', function(){
   //     debugger
-  //     expect(element.find('.key').text()).toContain('""');
+  //     expect(element.find('.key').text()).to.contain('""');
   //   });
   // });
 
@@ -119,10 +116,10 @@ describe('rendering', ()=> {
   //     element = createDirective('emptyArray');
   //   });
   //   it('should render "Array"', function(){
-  //     expect(element.text()).toContain('Array');
+  //     expect(formatter.render().innerText).to.contain('Array');
   //   });
   //   it('should have brackets and length: [0]', function(){
-  //     expect(element.text()).toContain('[0]');
+  //     expect(formatter.render().innerText).to.contain('[0]');
   //   });
   // });
 
@@ -131,10 +128,10 @@ describe('rendering', ()=> {
   //     element = createDirective('array');
   //   });
   //   it('should render "Array"', function(){
-  //     expect(element.text()).toContain('Array');
+  //     expect(formatter.render().innerText).to.contain('Array');
   //   });
   //   it('should have brackets and length: [3]', function(){
-  //     expect(element.text()).toContain('[3]');
+  //     expect(formatter.render().innerText).to.contain('[3]');
   //   });
   // });
 
@@ -143,11 +140,11 @@ describe('rendering', ()=> {
   //     element = createDirective('simpleObject');
   //   });
   //   it('should render "Object"', function(){
-  //     expect(element.text()).toContain('Object');
+  //     expect(formatter.render().innerText).to.contain('Object');
   //   });
   //   it('should open when clicking on "Object"', function(){
   //     element.find('.constructor-name').click();
-  //     expect(element.find('.toggler').hasClass('open')).toBe(true);
+  //     expect(element.find('.toggler').hasClass('open')).to.equal(true);
   //   });
   // });
 
@@ -155,7 +152,7 @@ describe('rendering', ()=> {
 
   //   it('default is disabled', function () {
   //     element = createDirective('mixArray');
-  //     expect(element.find('.thumbnail-text').length).toBe(0);
+  //     expect(element.find('.thumbnail-text').length).to.equal(0);
   //   });
 
   //   describe('set enable', function () {
@@ -165,22 +162,22 @@ describe('rendering', ()=> {
 
   //     it('should render "simple object"', function () {
   //       element = createDirective('simpleObject', 0);
-  //       expect(element.find('.thumbnail-text').text().trim()).toBe('{me:1}');
+  //       expect(element.find('.thumbnail-text').text().trim()).to.equal('{me:1}');
   //     });
 
   //     it('should render "longer object"', function () {
   //       element = createDirective('longerObject', 0);
-  //       expect(element.find('.thumbnail-text').text().trim()).toBe('{numbers:Array[3], boolean:true, null:null, number:123, anObject:Object…}');
+  //       expect(element.find('.thumbnail-text').text().trim()).to.equal('{numbers:Array[3], boolean:true, null:null, number:123, anObject:Object…}');
   //     });
 
   //     it('should render "array"', function () {
   //       element = createDirective('array', 0);
-  //       expect(element.find('.thumbnail-text').text().trim()).toBe('["one", "two", "three"]');
+  //       expect(element.find('.thumbnail-text').text().trim()).to.equal('["one", "two", "three"]');
   //     });
 
   //     it('should render "mixArray"', function () {
   //       element = createDirective('mixArray', 0);
-  //       expect(element.find('.thumbnail-text').text().trim()).toBe('[1, "2", Object]');
+  //       expect(element.find('.thumbnail-text').text().trim()).to.equal('[1, "2", Object]');
   //     });
   //   });
 
