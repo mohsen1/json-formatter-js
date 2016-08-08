4c1f803bcd51d06fdf14cd6264b3f187d61bad04
Mohsen Azimi <me@azimi.me>
2016-08-08 13:04:47 -0700

    Revert "Give proper name to webpack output"

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: 'dist',
-    filename: 'json-formatter.js',
+    filename: 'bundle.js',
     library: 'JSONFormatter',
     libraryTarget: 'umd',
     umdNamedDefine: true
