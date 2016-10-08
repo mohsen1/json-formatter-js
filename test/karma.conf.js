b388e1daa82a1b04b0a3e7a58d750f2723561e46
Mohsen Azimi <mazimi@lyft.com>
2016-10-08 11:05:14 -0700

    JS

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome' + process.env.TRAVIS ? 'Travis' : ''],
+    browsers: ['Chrome' + (process.env.TRAVIS ? 'Travis' : '')],
 
     customLaunchers: {
       ChromeTravis: {
